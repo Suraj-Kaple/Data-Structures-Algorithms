60b8f16d9a021dee8444dd07e7a78c44a14a7c94
Suraj-Kaple <kaplesuraj@gmail.com>
2021-06-29 13:00:02 +0530

    Create Pascal Triangle.cpp

---
+++ Arrays/Pascal Triangle.cpp
@@ -0,0 +1,16 @@
+https://www.interviewbit.com/problems/pascal-triangle/
+
+vector<vector<int> > Solution::solve(int A) {
+    vector<vector<int> > v(A);
+    if(A == 0)  return v;
+    v[0] = {1};
+    for(int i=1; i<A; i++){
+        v[i] = {1};
+        for(int j=1; j<i; j++){
+            int num = v[i-1][j] + v[i-1][j-1];
+            v[i].push_back(num);
+        }
+        v[i].push_back(1);
+    }
+    return v;
+}
